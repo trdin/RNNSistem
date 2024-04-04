26e73602425a43eb0094d4d714a174b2503ea32f
trdin <trdin.peter@gmail.com>
2024-04-04 20:29:01 +0200

    fetching data

---
+++ src/data/mbike_data.py
@@ -0,0 +1,84 @@
+import requests
+import csv
+import os
+from datetime import datetime
+import json
+import src.data.weather_data as wd
+
+# Function to fetch data from the API
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to fetch data:", response.status_code)
+        return None
+
+# Function to save raw JSON data
+def save_raw_data(data, filename):
+    with open(filename, 'w') as file:
+        file.write(json.dumps(data, indent=4))
+    print("Raw data saved to", filename)
+
+# Function to save data to CSV
+def save_to_csv(station, filename, timestamp):
+    weather_data = wd.fetch_and_write_weather_data(station['position']['lat'], station['position']['lng'], 'data/raw/weather/weather_data_' + station['name'] + '.json', timestamp=timestamp)
+    columns = ["date", "bike_stands", "available_bike_stands", "temperature", 
+               "relative_humidity", "dew_point", "apparent_temperature", 
+               "precipitation_probability", "rain", "surface_pressure"]
+    
+    weather_data = json.loads(weather_data)
+    
+    file_exists = os.path.exists(filename)
+
+    with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        
+        if not file_exists:
+            writer.writeheader()
+        
+        station_data = {
+            "date": timestamp,
+            "temperature": weather_data["temperature"], 
+            "relative_humidity": weather_data["relative_humidity"],
+            "dew_point": weather_data["dew_point"],
+            "apparent_temperature": weather_data["apparent_temperature"],
+            "precipitation_probability": weather_data["precipitation_probability"],
+            "rain": weather_data["rain"],
+            "surface_pressure": weather_data["surface_pressure"],
+            "bike_stands": station["bike_stands"],
+            "available_bike_stands": station["available_bike_stands"],
+        }
+        
+        writer.writerow(station_data)
+    
+    print("Data saved to", filename)
+    return weather_data
+
+# Main function
+def main():
+    try:
+        api_url = "https://api.jcdecaux.com/vls/v1/stations?contract=maribor&apiKey=5e150537116dbc1786ce5bec6975a8603286526b"
+        data = fetch_data(api_url)
+        if data:
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            # Save raw data
+            raw_filename = 'data/raw/mbajk/bike_data_' + timestamp + '.json'
+            save_raw_data(data, raw_filename)
+
+        
+            
+            weather_data = []
+
+            # Save processed data to CSV
+            for station in data:
+                filename = 'data/processed/' + station['name'] + '.csv'
+                weather_data.append(save_to_csv(station, filename, timestamp))
+            
+            save_raw_data(weather_data, 'data/raw/weather/weather_data_' + timestamp + '.json')
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ src/data/weather_data.py
@@ -0,0 +1,51 @@
+import openmeteo_requests
+import json
+import requests_cache
+from retry_requests import retry
+from datetime import datetime
+
+
+def fetch_and_write_weather_data(latitude, longitude, filename, timestamp):
+    print(latitude, longitude, filename)
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+    # Initialize the Open-Meteo API client
+
+    # Define the parameters for the weather data request
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": ["temperature_2m", "relative_humidity_2m", "dew_point_2m",
+                     "apparent_temperature", "precipitation_probability", "rain", "surface_pressure"],
+        "forecast_days": 1,
+    }
+
+    # Make the API request
+    responses = openmeteo.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
+
+    # Process the first response (assuming you're interested in the first location)
+    response = responses[0]
+
+    print(response)
+
+    # Extract hourly data
+    current = response.Current()
+    hourly_data = {
+        "date": timestamp,
+        "temperature": current.Variables(0).Value(),
+        "relative_humidity": current.Variables(1).Value(),
+        "dew_point": current.Variables(2).Value(),
+        "apparent_temperature": current.Variables(3).Value(),
+        "precipitation_probability": current.Variables(4).Value(),
+        "rain": current.Variables(5).Value(),
+        "surface_pressure": current.Variables(6).Value()
+    }
+
+    # Convert the data to JSON format
+    json_data = json.dumps(hourly_data, indent=4)
+
+
+
+    return json_data
+
